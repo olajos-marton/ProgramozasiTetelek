8dad717fa9929f51425ea7e1536a790743c7ad9d
Márton Olajos <olajos.marci@gmail.com>
2023-12-10 09:17:04 +0100

    Add project files.

---
+++ ProgramozasiTetelek/OsszegezhetoTomb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProgramozasiTetelek
+{
+    public class OsszegezhetoTomb
+    {
+        private readonly int[] adatok;
+
+        public OsszegezhetoTomb(int[] adatok) => this.adatok = adatok ?? throw new ArgumentNullException(nameof(adatok));
+
+        public int Osszeg
+        {
+            get
+            {
+                int osszeg = 0;
+                foreach (int adat in adatok)
+                {
+                    osszeg += adat;
+                }
+                return osszeg;
+            }
+        }
+    }
+}
---
+++ ProgramozasiTetelekTesztek/OsszegezhetoTombTesztek.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProgramozasiTetelek.Tesztek
+{
+    [TestClass]
+    public class OsszegezhetoTombTesztek
+    {
+        [TestMethod]
+        public void NullTomb()
+        {
+            int[] adatok = null;
+            Assert.ThrowsException<ArgumentNullException>(() => new OsszegezhetoTomb(adatok));
+        }
+
+        [TestMethod]
+        public void UresTombOsszege0()
+        {
+            int[] adatok = { };
+            var osszegezhetoTomb = new OsszegezhetoTomb(adatok);
+            int elvartOsszeg = 0;
+            int tenylegesOsszeg = osszegezhetoTomb.Osszeg;
+            Assert.AreEqual(elvartOsszeg, tenylegesOsszeg);
+        }
+
+        [TestMethod]
+        public void EgyelemuTombOsszegeAzElemErteke()
+        {
+            int[] adatok = { 7 };
+            var osszegezhetoTomb = new OsszegezhetoTomb(adatok);
+            Assert.AreEqual(7, osszegezhetoTomb.Osszeg);
+        }
+
+        [TestMethod]
+        public void PozitivElemek()
+        {
+            int[] adatok = { 1, 2, 3, 4, 5 };
+            var osszegezhetoTomb = new OsszegezhetoTomb(adatok);
+            Assert.AreEqual(15, osszegezhetoTomb.Osszeg);
+        }
+
+        [TestMethod]
+        public void NegativElemek()
+        {
+            int[] adatok = { -1, -2, -3, -4, -5 };
+            var osszegezhetoTomb = new OsszegezhetoTomb(adatok);
+            Assert.AreEqual(-15, osszegezhetoTomb.Osszeg);
+        }
+
+        [TestMethod]
+        public void VegyesenPozitivEsNegativElemek()
+        {
+            int[] adatok = { 1, -2, 3, -4, 5 };
+            var osszegezhetoTomb = new OsszegezhetoTomb(adatok);
+            Assert.AreEqual(3, osszegezhetoTomb.Osszeg);
+        }
+    }
+}
